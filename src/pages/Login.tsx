6dbf7d93df078182a09131e6b35f539f933667b8
Lucas <lucas@Lucass-MacBook-Pro.local>
2023-01-16 12:49:23 -0300

    config: react router dom

---
+++ src/pages/Login.tsx
@@ -0,0 +1,10 @@
+export function Login() {
+
+    return (
+        <div>
+            <input type="text" />
+            <input type="password" />
+            <button>Entrar</button>
+        </div>
+    )
+}
